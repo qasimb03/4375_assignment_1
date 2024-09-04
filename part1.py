3ca0152b0fcd8962ac8f091a9cb098cb14656702
qasimb03 <4qasim@gmail.com>
2024-09-04 16:07:55 -0500

    Imports

--- part1.py
+++ part1.py
@@ -1,9 +1,14 @@
 '''
-Qasim Bhutta & Cameron 
+Qasim Bhutta & Cameron [LastName]
 Assignment 1
 Anurag Nagar
 CS 4375
 '''
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
 
 # Fetch dataset
 from ucimlrepo import fetch_ucirepo 
@@ -14,9 +19,8 @@ student_performance = fetch_ucirepo(id=320)
 # data (as pandas dataframes) 
 X = student_performance.data.features 
 y = student_performance.data.targets 
-  
-# metadata 
-print(student_performance.metadata) 
-  
+
+print(X.columns)
+
 # variable information 
-print(student_performance.variables) 
+# print(student_performance.variables) 
